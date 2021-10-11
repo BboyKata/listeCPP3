f10fa6beaa3c2fe65247f114fc915b8611c80d39
Bboy Kata <72092989+BboyKata@users.noreply.github.com>
2021-10-11 20:48:12 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,7 +14,75 @@ void initLista(nodo **T, nodo **coda){
     cout<<endl<<"|||| LISTA CREATA ||||"<<endl;
 }
 
-void headAdd(int x = 0, int v = 0, nodo **T = NULL, nodo **coda = NULL){
+bool findList(nodo **T, int s){
+    nodo *p = *T;
+    bool found = false;
+    while(p != NULL && !found){
+        if(p->x == s){
+            found = true;
+        }
+        p = p->next;
+    }
+    return found;
+}
+
+bool isEmpty(nodo **T){
+    bool isempty;
+    if(*T == NULL){
+        isempty = true;
+    } else {
+        isempty = false;
+    }
+    return isempty;
+}
+
+int elemLista(nodo** T){
+    int cont=0;
+    nodo *p = *T;
+    while(p != NULL){
+        cont++;
+        p = p->next;
+    }
+    return cont;
+}
+
+void popList(nodo** T,nodo** coda,int e, int x){
+    nodo *p = *T;
+    nodo *i = new nodo;
+    nodo *j = new nodo;
+    if(e >= elemLista(T)){
+        cout<<"Errore, non esiste un nodo all'indice "<<e<<" !!!"<<endl<<endl;
+    }else if(isEmpty(T)){
+        cout<<"Errore, lista non ancora inizializzata "<<" !!!"<<endl<<endl;
+    } else {
+        if(e==0){
+            p = (*T)->next;
+            (*T)->next = NULL;
+            (*T)->x = NULL;
+            *T = p;
+        } else {
+            for(int i = 0;i<e-1;i++){
+                p = p->next;
+            }
+            i = p->next;
+            j = i->next;
+            (i)->next = NULL;
+            (i)->x = NULL;
+
+            if(isEmpty(&j)){
+                p->next = NULL;
+                *coda = p;
+            }else{
+                p->next = j;
+            }
+        }
+        if(x != 0){
+            cout<<endl<<"|||| ELEMENTO RIMOSSO ALL'INDICE "<<e<<" ||||"<<endl;
+        }
+    }
+}
+
+void headAdd(int x, int v, nodo **T, nodo **coda){
     nodo *nuovoP = new nodo;
     if(x != 0){
         cout<<"Inserisci il valore del nodo da aggiungere in testa: ";
@@ -24,7 +92,7 @@ void headAdd(int x = 0, int v = 0, nodo **T = NULL, nodo **coda = NULL){
     }
 
     nuovoP->next = NULL;
-    if(T == NULL){
+    if(isEmpty(T)){
         *T = nuovoP;
         *coda = nuovoP;
     } else {
@@ -33,10 +101,11 @@ void headAdd(int x = 0, int v = 0, nodo **T = NULL, nodo **coda = NULL){
     }
     if( x != 0){
         cout<<endl<<"|||| NODO AGGIUNTO IN TESTA ||||"<<endl;
+        cout<<"|||| NUMERO NODI ATTUALI: "<<elemLista(T)<<" ||||"<<endl;
     }
 }
 
-void tailAdd(int x = 0, int v = 0, nodo **T = NULL, nodo **coda = NULL){
+void tailAdd(int x, int v, nodo **T, nodo **coda){
     nodo *nuovoP = new nodo;
     if(x != 0){
         cout<<"Inserisci il valore del nodo da aggiungere in coda: ";
@@ -45,7 +114,7 @@ void tailAdd(int x = 0, int v = 0, nodo **T = NULL, nodo **coda = NULL){
         nuovoP->x = v;
     }
     nuovoP->next = NULL;
-    if(*T == NULL){
+    if(isEmpty(T)){
         *T = nuovoP;
         *coda = nuovoP;
     } else {
@@ -55,6 +124,7 @@ void tailAdd(int x = 0, int v = 0, nodo **T = NULL, nodo **coda = NULL){
     }
     if(x != 0){
         cout<<endl<<"|||| NODO AGGIUNTO IN CODA ||||"<<endl;
+        cout<<"|||| NUMERO NODI ATTUALI: "<<elemLista(T)<<" ||||"<<endl;
     }
 }
 
@@ -79,13 +149,58 @@ void faiUnPoTu(nodo **T, nodo **coda){
     stampaLista(T,coda);
 }
 
+void sortList(nodo **T){
+    if(isEmpty(T)){
+        cout<<"Errore, non posso ordinare una lista vuota!!!"<<endl;
+    } else{
+
+        nodo *p = *T;
+        nodo *i = *T;
+        nodo *j = (*T)->next;
+        int temp;
+        while(p != NULL){
+            while(j != NULL){
+                if(i->x>j->x){
+                    temp = i->x;
+                    i->x = j->x;
+                    j->x = temp;
+                    }
+                j = j->next;
+                i = i->next;
+                }
+            p = p->next;
+            i= *T;
+            j = (*T)->next;
+        }
+        cout<<endl<<"|||| LISTA ORDINATA ||||"<<endl;
+    }
+
+}
+
+void reverseList(nodo **T, nodo**coda){
+    int temp;
+    nodo *p = *T;
+    nodo *j = new nodo;
+    if(isEmpty(T)){
+        cout<<"Errore, lista non ancora inizializzata "<<" !!!"<<endl<<endl;
+    } else {
+        for(int i=0;i<elemLista(T)-1;i++){
+            j = p->next;
+            temp = j->x;
+            popList(T,coda,i+1,0);
+            headAdd(0,temp,T,coda);
+        }
+    }
+}
+
 int main()
 {
-    int c;
+    int c,s;
     nodo *T, *coda;
     do{
         cout<<"Operazioni sulla lista:"<<endl;
-        cout<<"0) Esci;\n1) Crea lista;\n2) Aggiungi elemento in testa;\n3) Aggiungi elemento in coda;\n4) Visualizza lista;\nInserisci scelta: ";
+        cout<<"0) Esci;\n1) Crea lista;\n2) Aggiungi elemento in testa;\n3) Aggiungi elemento in coda;\n4) Visualizza lista;\n";
+        cout<<"5) TestLista;\n6) Ricerca valore nella lista;\n7) Ordina lista;\n8) Rimuovi nodo dalla lista;\n9) Inverti lista;\nInserisci scelta: ";
         cin>>c;
         system("cls");
         switch(c){
@@ -106,6 +221,26 @@ int main()
                 break;
             case 5:
                 faiUnPoTu(&T,&coda);
+                break;
+            case 6:
+                cout<<"Inserisci il valore da ricercare nella lista: ";
+                cin>>s;
+                if(findList(&T,s)){
+                    cout<<"Elemento trovato!"<<endl;
+                } else {
+                    cout<<"Elemento non trovato!"<<endl;
+                }
+                break;
+            case 7:
+                sortList(&T);
+                break;
+            case 8:
+                cout<<"Inserisci l'indice dell'elemento da rimuovere[0-"<<elemLista(&T)-1<<"]: ";
+                cin>>s;
+                popList(&T,&coda,s,1);
+                break;
+            case 9:
+                reverseList(&T,&coda);
                 break;
             default:
                 cout<<"Errore, reinserire la scelta."<<endl;
